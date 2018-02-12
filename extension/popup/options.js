329468f414d6d3e10f66f619bb268ef1a6a25f31
Carlos Puchol <cpg+git@amahi.org>
2018-02-12 03:03:25 -0800

    work in progress towards a popup menu

---
+++ extension/popup/options.js
@@ -0,0 +1,36 @@
+
+/**
+ * Listen for clicks on the buttons, and send the appropriate message to
+ * the content script in the page.
+ */
+function listenForClicks() {
+  document.addEventListener("click", (e) => {
+
+    /**
+     * Just log the error to the console.
+     */
+    function reportError(error) {
+      console.error(`Could not run: ${error}`);
+    }
+
+  });
+}
+
+/**
+ * There was an error executing the script.
+ * Display the popup's error message, and hide the normal UI.
+ */
+function reportExecuteScriptError(error) {
+  document.querySelector("#popup-content").classList.add("hidden");
+  document.querySelector("#error-content").classList.remove("hidden");
+  console.error(`Failed to execute add-on content script: ${error.message}`);
+}
+
+/**
+ * When the popup loads, inject a content script into the active tab,
+ * and add a click handler.
+ * If we couldn't inject the script, handle the error.
+ */
+browser.tabs.executeScript({code: ''})
+.then(listenForClicks)
+.catch(reportExecuteScriptError);
